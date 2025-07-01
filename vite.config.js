98c02897b3291af35c20e5be5ae097101f401da3
mirzaatifbaig <mirzaatifb91@gmail.com>
2025-07-01 23:55:59 +0500

    Made changes to publish on github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import {defineConfig} from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/react-query/',
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
